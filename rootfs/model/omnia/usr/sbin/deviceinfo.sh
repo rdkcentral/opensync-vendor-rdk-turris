93b3bcd43bc0618d5963659429b29d66b3c297a7
manigandan.gopalakrishnan <manigandan.gopalakrishnan@ltts.com>
2020-03-20 14:03:35 +0530

    Handling Turris-Extender specifics in deviceinfo.sh
    Signed-off-by: manigandan.gopalakrishnan <manigandan.gopalakrishnan@ltts.com>

--- rootfs/model/omnia/usr/sbin/deviceinfo.sh
+++ rootfs/model/omnia/usr/sbin/deviceinfo.sh
@@ -2,67 +2,90 @@
 
 # TODO: Implement missing options
 
+device_type=`cat /version.txt | grep imagename | cut -d':' -f2 | cut -d'-' -f3`
 case "$1" in
 
-    -mo)
-	echo "RTROM01-2G"
-#        echo "option $1 is not implemented" ; exit 1  # TODO
-        ;;
-    -sn)
-        echo "1122334455"
-#        echo "option $1 is not implemented" ; exit 1  # TODO
-        ;;
-    -fw)
-        #cho "option $1 is not implemented" ; exit 1  # TODO
-	echo "rdk-yocto-turris-1"
-        ;;
-    -cmac)
-#        echo "option $1 is not implemented" ; exit 1  # TODO
-	echo $(cat /sys/class/net/lan0/address)
-        ;;
-    -cip)
-#        echo "option $1 is not implemented" ; exit 1  # TODO
-	echo $(ip addr show lan0 | grep 'inet ' | awk '{print $2}' | cut -f1 -d'/')
-        ;;
-    -cipv6)
-#       echo "option $1 is not implemented" ; exit 1  # TODO
-	echo ""
-        ;;
-    -emac)
-#        echo "option $1 is not implemented" ; exit 1  # TODO
-	echo $(cat /sys/class/net/erouter0/address)
-        ;;
-    -eip)
-#       echo "option $1 is not implemented" ; exit 1  # TODO
-	echo $(ip addr show erouter0 | grep 'inet ' | awk '{print $2}' | cut -f1 -d'/')
-        ;;
-    -eipv6)
-#       echo "option $1 is not implemented" ; exit 1  # TODO
-	echo ""
-        ;;
-    -lmac)
-        #echo "option $1 is not implemented" ; exit 1  # TODO
-        echo $(cat /sys/class/net/lan0/address)
-        ;;
-    -lip)
-        #echo "option $1 is not implemented" ; exit 1  # TODO
-        echo $(ip addr show lan0 | grep 'inet ' | awk '{print $2}' | cut -f1 -d'/')
-        ;;
-    -lipv6)
-	#      echo "option $1 is not implemented" ; exit 1  # TODO
-	echo ""
-        ;;
-    -ms)
-	#echo "option $1 is not implemented" ; exit 1  # TODO
-	echo "Full"
-        ;;
-    -mu)
-	echo "ssl:wildfire.plume.tech:443"
-#        echo "option $1 is not implemented" ; exit 1  # TODO
-        ;;
+	-mo)
+		if [ $device_type == "extender" ]; then
+			echo "RTROM01-2G-EX"
+		else
+			echo "RTROM01-2G"
+		fi
+		;;
+	-sn)
+		if [ $device_type == "extender" ]; then
+			echo "5544332211"
+		else
+			echo "1122334455"
+		fi
+		#echo "option $1 is not implemented" ; exit 1  # TODO need to fetch actual serial number
+		;;
+	-fw)
+		echo "rdk-yocto-turris-1"
+		#echo "option $1 is not implemented" ; exit 1  # TODO
+		;;
+	-cmac)
+		#MAC address of eth1 is used to register device with Plume NOC
+		echo $(cat /sys/class/net/eth1/address)
+		;;
+	-cip)
+		if [ $device_type == "extender" ]; then
+			echo $(ip addr show br-home | grep 'inet ' | awk '{print $2}' | cut -f1 -d'/')
+		else
+			echo $(ip addr show brlan0 | grep 'inet ' | awk '{print $2}' | cut -f1 -d'/')
+		fi
+		;;
+	-cipv6)
+		echo ""
+		#echo "option $1 is not implemented" ; exit 1  # TODO
+		;;
+	-emac)
+		if [ $device_type == "extender" ]; then
+			echo $(cat /sys/class/net/br-wan/address)
+		else
+			echo $(cat /sys/class/net/erouter0/address)
+		fi
+		;;
+	-eip)
+		if [ $device_type == "extender" ]; then
+			echo $(ip addr show br-wan | grep 'inet ' | awk '{print $2}' | cut -f1 -d'/')
+		else
+			echo $(ip addr show erouter0 | grep 'inet ' | awk '{print $2}' | cut -f1 -d'/')
+		fi
+		;;
+	-eipv6)
+		echo ""
+		#echo "option $1 is not implemented" ; exit 1  # TODO
+		;;
+	-lmac)
+		if [ $device_type == "extender" ]; then
+			echo $(cat /sys/class/net/br-home/address)
+		else
+			echo $(cat /sys/class/net/brlan0/address)
+		fi
+		;;
+	-lip)
+		if [ $device_type == "extender" ]; then
+			echo $(ip addr show br-home | grep 'inet ' | awk '{print $2}' | cut -f1 -d'/')
+		else
+			echo $(ip addr show brlan0 | grep 'inet ' | awk '{print $2}' | cut -f1 -d'/')
+		fi
+		;;
+	-lipv6)
+		echo ""
+		#echo "option $1 is not implemented" ; exit 1  # TODO
+		;;
+	-ms)
+		echo "Full"
+		#echo "option $1 is not implemented" ; exit 1  # TODO
+		;;
+	-mu)
+		echo "ssl:wildfire.plume.tech:443"
+		#echo "option $1 is not implemented" ; exit 1  # TODO
+		;;
 
-    *)
-        echo "Usage: deviceinfo.sh [-mo|-sn|-fw|-cmac|-cip|-cipv6|-emac|-eip|-eipv6|-lmac|-lip|-lipv6|-ms|-mu]"
-        exit 1
-        ;;
+	*)
+		echo "Usage: deviceinfo.sh [-mo|-sn|-fw|-cmac|-cip|-cipv6|-emac|-eip|-eipv6|-lmac|-lip|-lipv6|-ms|-mu]"
+		exit 1
+		;;
 esac
